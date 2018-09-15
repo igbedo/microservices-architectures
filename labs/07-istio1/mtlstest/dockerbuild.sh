7a0060e13c67a320be58641e6025d51bf066963e
Otelahu Igbe <otelahu@cloudtechnologyexperts.com>
2018-09-15 17:50:32 -0500

    Remove duplicated directory

--- labs/07-istio1/mtlstest/dockerbuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -t mtlstest .
-docker tag mtlstest gcr.io/$PROJECT_ID/mtlstest:latest
-gcloud docker -- push gcr.io/$PROJECT_ID/mtlstest:latest
--- labs/07-istio1/mtlstest/k8ssetup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl create -f <(istioctl kube-inject -f mtlstest.yaml) --validate=true --dry-run=false
